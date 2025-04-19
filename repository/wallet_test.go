f82f18ac88aab7d1020d96bf188240bba31d459d
Aswadh Puthen Veede <98012322+Aswadhpv@users.noreply.github.com>
2025-04-19 15:04:30 +0700

    add repository tests
    Unit tests for ApplyOperation using sqlmock

---
+++ repository/wallet_test.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"context"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+)
+
+func setupMock(t *testing.T) (*walletRepo, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("failed to open mock db: %v", err)
+	}
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+	return &walletRepo{db: sqlxDB}, mock
+}
+
+func TestDeposit_NewWallet(t *testing.T) {
+	repo, mock := setupMock(t)
+	mock.ExpectBegin()
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT balance")).
+		WithArgs("abc").
+		WillReturnError(sql.ErrNoRows)
+	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO wallets")).
+		WithArgs("abc", int64(100)).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	err := repo.ApplyOperation(context.Background(), "abc", "DEPOSIT", 100)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
