1069db63481a579fbafc2f1d1f560d4b8f98db22
Aswadh Puthen Veede <98012322+Aswadhpv@users.noreply.github.com>
2025-04-19 14:54:14 +0700

    add config package
    Load database and application port settings from config.env

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	AppPort    string
+}
+
+func Load() *Config {
+	if err := godotenv.Load("config.env"); err != nil {
+		log.Printf("No .env file found, reading environment variables directly")
+	}
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		AppPort:    os.Getenv("APP_PORT"),
+	}
+}
